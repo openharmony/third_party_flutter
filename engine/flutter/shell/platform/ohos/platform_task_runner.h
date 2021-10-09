03c876b043cf2e9e865730a045e86d17b5677f53
dujingcheng <dujingcheng@huawei.com>
2021-10-09 01:57:20 +0000

    dujingcheng@huawei.com
    Signed-off-by: dujingcheng <dujingcheng@huawei.com>

---
+++ engine/flutter/shell/platform/ohos/platform_task_runner.h
@@ -0,0 +1,22 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// 2021.10.08 platform task runner adapt ohos.
+//           Copyright (c) 2021 Huawei Device Co., Ltd. All rights reserved.
+
+#ifndef FLUTTER_SHELL_PLATFORM_OHOS_PLATFORM_TASK_RUNNER_H
+#define FLUTTER_SHELL_PLATFORM_OHOS_PLATFORM_TASK_RUNNER_H
+
+#include "flutter/fml/message_loop_impl.h"
+#include "flutter/fml/message_loop_task_queues.h"
+
+namespace flutter {
+
+class PlatformTaskRunner {
+public:
+    static fml::RefPtr<fml::TaskRunner> CurrentTaskRunner();
+};
+
+}  // namespace flutter
+
+#endif // FLUTTER_SHELL_PLATFORM_OHOS_PLATFORM_TASK_RUNNER_H
