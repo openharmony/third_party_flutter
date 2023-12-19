868471e0b12cf7133f9a61ef94221f19b7e9a9e8
wenfei6316 <wenfei6316@163.com>
2023-12-19 20:02:06 +0800

    add riscv64 arch compiler adapt
    Signed-off-by: wenfei6316 <wenfei6316@163.com>
    Change-Id: Ic19e1ff4bc6603fb787e8faa9e140fc6fdac93f2

--- engine/flutter/fml/build_config.h
+++ engine/flutter/fml/build_config.h
@@ -103,6 +103,11 @@
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
 #endif
+#elif defined(__riscv) && (__riscv_xlen == 64)
+#define ARCH_CPU_RISCV_FAMILY 1
+#define ARCH_CPU_RISCV64 1
+#define ARCH_CPU_64_BITS 1
+#define ARCH_CPU_LITTLE_ENDIAN 1
 #else
 #error Please add support for your architecture in flutter/fml/build_config.h
 #endif
